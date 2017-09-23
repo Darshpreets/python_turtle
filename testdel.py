b50815ebad9a75c17889fb65627acb4edc97a9f4
darhpreets <mrdsinghh@gmail.com>
2017-09-23 14:27:50 +0530

    modidification in user input

--- testdel.py
+++ testdel.py
@@ -1,10 +1,18 @@
+#! /usr/bin/python
+
 
 import turtle 
-
+wn = turtle.Screen() 
 polygon = turtle.Turtle()
 
-num_sides = 6
-side_length = 70
+num_sides = input("Enter Number of sides of polygon: ")
+side_length = input("Enter lenghth of sides of polygon: should be more than 0!!  ")
+colors = input("Enter the color of side: [example blue, green, white, yellow]; It must be in quotes: " )
+
+polygon.color(colors)              # make color of polygon
+polygon.pensize(3)                 # set the width of her pen
+
+
 angle = 360.0 / num_sides 
 
 for i in range(num_sides):
